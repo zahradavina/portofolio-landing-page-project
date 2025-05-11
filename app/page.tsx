ffffeed12fc8cca19250c6707f3e75efc43785a0
Zahra Davina B <136558346+zahradavina@users.noreply.github.com>
2025-05-11 18:48:38 +0700

    This is a portofolio landing page projects
    This Portofolio landing page project are built using Next.js + reactbits.

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,77 @@
 import Image from "next/image";
+import Lanyard from "./components/Components/Lanyard/Lanyard";
+import DecryptedText from "./components/TextAnimations/DecryptedText/DecryptedText";
+import BlurText from "./components/TextAnimations/BlurText/BlurText";
+import AnimatedContent from "./components/Animations/AnimatedContent/AnimatedContent";
+import Aurora from "./components/Backgrounds/Aurora/Aurora";
+import ClickSpark from "./components/Animations/ClickSpark/ClickSpark";
+import ShinyText from "./components/TextAnimations/ShinyText/ShinyText";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  function handleAnimationComplete(): void {
+    throw new Error("Function not implemented.");
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  return (
+    <div className ="min-h-screen overflow-x-hidden bg-[#1a1a1a]">
+      <ClickSpark />
+   <div className="absolute top-0 right-0 left-0 bottom-0 w-full h-full"> 
+    <Aurora 
+    colorStops={["#00cc99","#f2f2f2","#ff4dff"]}
+    blend={0.5}
+    amplitude={1.0}
+    speed={0.5}/>
     </div>
+    <div className="container mx-auto h-screen">
+      <div className="grid grid-cols-12">
+        <div className="col-span-6">
+          <div className="flex items-center h-full">
+            <div className="flex flex-col gap-6">
+              <AnimatedContent 
+              distance={150}
+              direction="vertical"
+              reverse={false}
+              config={{ tension: 80, friction: 20 }}
+              initialOpacity={0.2}
+              animateOpacity
+              scale={1.1}
+              threshold={0.2}>
+                  <h1 className="text-2xl text-amber-100 font-bold">Davin's Portofolio</h1>
+                      <DecryptedText text="Hello, I'm Zahra Davina B!" 
+                        speed={50} 
+                        maxIterations={10} 
+                        sequential={true} 
+                        revealDirection="start" 
+                        useOriginalCharsOnly={false} 
+                        characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+" 
+                        className="text-5xl text-pink-300 font-bold" />
+
+                      <DecryptedText text="I am an Electrical Engineer" 
+                        speed={50} 
+                        maxIterations={10} 
+                        sequential={true} 
+                        revealDirection="start" 
+                        useOriginalCharsOnly={false} 
+                        haracters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+" 
+                        className="text-3xl text-amber-100 font-semibold" />
+                      <BlurText
+                        text="Welcome to my portofolio page! You can called me Davin, currently working as a professional clown and addicted to coffee.🤡☕️"
+                        delay={150}
+                        animateBy="words"
+                        direction="top"
+                        className="text-xl mb-8" />
+                        <div className="flex items-center gap-4">
+                        <a href ="mailto:zahradavinab@gmail.com"><ShinyText text="Contact Me!" disabled={false} speed={5} className='custom-class border-2 px-4 py-2 rounded-3xl' /></a>
+                        </div>
+                </AnimatedContent>
+              </div>
+           </div>
+        </div>
+          <div className="col-span-6">
+          <Lanyard position={[0,0,12]} gravity={[0,-40,0]}/>
+          </div>
+          </div>
+        </div>
+      </div>
   );
 }
